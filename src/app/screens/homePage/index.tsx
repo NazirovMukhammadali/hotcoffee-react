1278fc9ea5d512ed7e9b70d0df0ac82569525a86
NazirovMukhammadali <nazirovali12345@gmail.com>
2025-08-21 19:58:57 +0900

    feat: develop slice and selector usage

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Statistics from "./Statistics";
 import PopularDishes from "./PopularDishes";
 import NewDishes from "./NewDishes";
 import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
-import "../../../css/home.css"
+import "../../../css/home.css";
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { setPopularDishes } from "./slice";
+import { retrievePopularDishes } from "./selector";
+import { Product } from "../../../lib/types/product";
+
+// REDUX SLICE & SELECTOR
+const actionDispatch = (dispatch: Dispatch) => ({
+    setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+});
+const popularDishesRetriever = createSelector(
+    retrievePopularDishes,
+    (popularDishes) => ({ popularDishes }))
 
 export default function HomePage() {
-    return <div className="homepage">
-        <Statistics />
-        <PopularDishes />
-        <NewDishes />
-        <Advertisement />
-        <ActiveUsers />
-        <Events />
-    </div>;
-}
+    const { setPopularDishes } = actionDispatch(useDispatch());
+    const { popularDishes } = useSelector(popularDishesRetriever)
+    //Select: Store => Data
+
+    useEffect(() => {
+        //Backend  server data request => Data
+    }, [])
+    console.log("popularDishes", popularDishes);
+    return (
+        <div className={"homepage"}>
+            <Statistics />
+            <PopularDishes />
+            <NewDishes />
+            <Advertisement />
+            <ActiveUsers />
+            <Events />
+        </div>
+    );
+} 
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,14 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import HomePageReducer from "./screens/homePage/slice";
+import reduxLogger from "redux-logger";
 
 export const store = configureStore({
-  reducer: {},
+  middleware: (getDefaultMiddleware) =>
+    // @ts-ignore
+    getDefaultMiddleware().concat(reduxLogger), //  cast qildik
+  reducer: {
+    homePage: HomePageReducer,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
